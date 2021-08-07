9a9d769e12bc2f7ba69c8dc897d098acbf2b13c3
Razvy Cretu <razvan.cretu97@gmail.com>
2021-08-08 02:14:56 +0300

    more cleaning

--- Client.js
+++ Client.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
 const Discord = require("discord.js");
 const { prefix } = require("./config.json");
-const dotenv = require("dotenv");
-dotenv.config();
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
--- MyBot.js
+++ MyBot.js
@@ -1,16 +1,16 @@
+require("dotenv").config();
 require("./Client.js");
 
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv");
-dotenv.config();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "/public"));
-app.get("/", (req, res) => {
-  res.sendFile("index.html");
-});
-app.listen(port, () => {
-  console.log("App up!");
-});
+app
+  .use(express.static(__dirname + "/public"))
+  .get("/", (req, res) => {
+    res.sendFile("index.html");
+  })
+  .listen(port, () => {
+    console.log("App up!");
+  });
